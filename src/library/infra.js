8182425bc633d611da47533a2e3c59aac52f4c08
Yutaka HARA <yutaka.hara.gmail.com>
2011-11-24 16:47:34 +0900

    assert_symbol (etc.) can take extra argument 'fname'
    eg.
      assert_symbol(ar[0], dynamically_defined_function_name);

--- src/library/infra.js
+++ src/library/infra.js
@@ -88,8 +88,9 @@ var make_assert = function(check){
     check.apply(this, [fname].concat(_.toArray(arguments)));
   }
 }
-var make_simple_assert = function(type, test){
+var make_simple_assert = function(type, test, _fname){
   return make_assert(function(fname, obj, opt){
+    if(_fname) fname = _fname;
     option = opt ? ("("+opt+")") : ""
     if(!test(obj)){
       throw new BiwaScheme.Error(fname + option + ": " +
